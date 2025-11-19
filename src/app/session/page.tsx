a4cff52feb17f781b52fe2c9e39733366a852460
Thanh Dong <dongnt.22ite@vku.udn.vn>
2025-11-19 16:04:05 +0700

    update some styles

--- src/app/session/page.tsx
+++ src/app/session/page.tsx
@@ -11,7 +11,11 @@ export default function FocusPage() {
     <div className='flex h-screen bg-gray-50 text-gray-800 font-sans'>
       <SidebarNav />
       <div className='flex-1 flex flex-col overflow-hidden'>
-        <HeaderBar title='Focus' isShowSearchBar={false} isShowNotification={true} />
+        <HeaderBar
+          title='Focus'
+          isShowSearchBar={false}
+          isShowNotification={true}
+        />
         <main className='flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 p-4 md:p-6 lg:p-8'>
           <div className='max-w-7xl mx-auto space-y-6'>
             {/* Timer Section */}
@@ -25,4 +29,3 @@ export default function FocusPage() {
     </div>
   );
 }
-
--- src/services/timerSession.ts
+++ src/services/timerSession.ts
@@ -1,17 +1,19 @@
-import { apiClient } from "@/lib/api";
-import { CreateTimerSessionData } from "@/mutations";
-import { TimerSession } from "@/types";
+import { apiClient } from '@/lib/api';
+import { CreateTimerSessionData } from '@/mutations';
+import { TimerSession } from '@/types';
 
 const endpoint = {
-    timerSession: '/timer-session',
+  timerSession: '/timer-session',
 };
 
-export const createTimerSession = async (data: CreateTimerSessionData): Promise<TimerSession> => {
-    try {
-        const response = await apiClient.post(endpoint.timerSession, data);
-        return response.data;
-    } catch (err) {
-        console.log('Error creating timer session: ', err);
-        throw err;
-    }
-}
+export const createTimerSession = async (
+  data: CreateTimerSessionData
+): Promise<TimerSession> => {
+  try {
+    const response = await apiClient.post(endpoint.timerSession, data);
+    return response.data;
+  } catch (err) {
+    console.log('Error creating timer session: ', err);
+    throw err;
+  }
+};
