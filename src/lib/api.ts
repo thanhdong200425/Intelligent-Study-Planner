4cf1d1696eeb5a5ea8df4d546d5323692e5f1fa1
Thanh Dong <dongnt.22ite@vku.udn.vn>
2025-12-08 03:48:12 +0000

    update to show toast if login credentials are not true

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -80,6 +80,8 @@ apiClient.interceptors.response.use(
   },
   async error => {
     const originalRequest = error.config;
+    if (originalRequest.url?.includes('/auth/login')) return Promise.reject(error);
+    
     if (error.response?.status === 401 && !originalRequest._retry) {
       if (isRefreshing) {
         return new Promise((resolve, reject) => {
